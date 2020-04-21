48d281c834122b982ff9223bb1888b14ab6d8257
Natalie Michelsen <natalie.michelsen@outlook.com>
2020-04-21 13:06:15 -0600

    Outline Commit

--- script.js
+++ script.js
@@ -1,3 +1,16 @@
+$(document).ready(function(){
+    $("#").on("click", function(){
+        var x = $("#x").val ();
+
+        $("#x").val("");
+
+        searchWeather(x);
+    });
+
+    $(".#").on("click", "li", function(){
+        searchWeather($(this).text()); 
+    });
+});
 function searchWeather
 function getForcast
 function getUVIndex
